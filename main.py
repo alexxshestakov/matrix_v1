1f360d6ae48464eaab29f974492adacaeec7f4c5
Alex <alexandershestak0v@yandex.ru>
2021-10-28 21:58:43 +0500

    #matrix_screen1.2

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ class MatrixLetters:
         self.font_size = 16
         self.font = pg.font.SysFont('arial',self.font_size, bold = True)
 
-        # self.letters = [chr(int('0x30a0', 16) + i) for i in range(1, 95)]
-        # self.font_size = 15
-        # self.font = pg.font.SysFont('ms mincho', self.font_size, bold=True)
-        # self.font = pg.font.Font('MS Mincho.ttf', self.font_size, bold=True)
+        self.letters = [chr(int('0x30a0', 16) + i) for i in range(1, 95)]
+        self.font_size = 15
+        self.font = pg.font.SysFont('ms mincho', self.font_size, bold=True)
+        self.font = pg.font.Font('MS Mincho.ttf', self.font_size, bold=True)
 
-        self.colums = app.WIDTH // self.font_size
-        self.drops = [1 for i in range(0,self.colums)]
+        # self.colums = app.WIDTH // self.font_size
+        # self.drops = [1 for i in range(0,self.colums)]
 
     def draw(self):
         for i in range(0,len(self.drops)):
